5f0a71af0b5c05ec66b8541a5597d15232a86108
Decartinator <2544636@students.wits.ac.za>
2024-04-10 21:13:08 +0200

    commit

--- api/cars/index.js
+++ api/cars/index.js
@@ -1,13 +1,69 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    console.log(req);
+    // Import cars data
+    const cars = require('./cars.json');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    // Enable CORS
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+            "Access-Control-Allow-Headers": "Content-Type"
+        }
     };
-}
+
+    // Extract request method and path
+    const { method, path } = req;
+
+    // Route requests based on method and path
+    switch (method) {
+        case 'GET':
+            if (path === '/cars') {
+                context.res.body = cars;
+            } else if (path.startsWith('/cars/')) {
+                const id = path.substring(6);
+                const car = cars.find(car => car.id === id);
+                context.res.body = car || "Car not found";
+            }
+            break;
+        case 'POST':
+            if (path === '/cars') {
+                const newCar = req.body;
+                cars.push(newCar);
+                context.res.body = newCar;
+            }
+            break;
+        case 'PUT':
+            if (path.startsWith('/cars/')) {
+                const id = path.substring(6);
+                const index = cars.findIndex(car => car.id === id);
+                if (index !== -1) {
+                    const updatedCar = req.body;
+                    cars[index] = updatedCar;
+                    context.res.body = updatedCar;
+                } else {
+                    context.res.status = 404;
+                    context.res.body = "Car not found";
+                }
+            }
+            break;
+        case 'DELETE':
+            if (path.startsWith('/cars/')) {
+                const id = path.substring(6);
+                const index = cars.findIndex(car => car.id === id);
+                if (index !== -1) {
+                    cars.splice(index, 1);
+                    context.res.body = { message: `Car with id ${id} deleted` };
+                } else {
+                    context.res.status = 404;
+                    context.res.body = "Car not found";
+                }
+            }
+            break;
+        default:
+            context.res.status = 405;
+            context.res.body = "Method not allowed";
+            break;
+    }
+    console.log("hello");
+};
